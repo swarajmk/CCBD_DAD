38cc410bce4530882b717b80c53654b5f187a72a
swarajmk <swarajkumaran@gmail.com>
2025-02-23 15:23:40 +0530

    Added avatar and character sheet

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,90 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export let initialText = 'Press any key to continue... ';
+
+export let gameLogo = `
+██████╗░░█████╗░██████╗░
+██╔══██╗██╔══██╗██╔══██╗
+██║░░██║███████║██║░░██║
+██║░░██║██╔══██║██║░░██║
+██████╔╝██║░░██║██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░
+
+Dungeons, AI & Dragons.`;
+
+export let characterSheetPrompt = `You are a dungeon master for a D&D game, For the given CHaracter Description, generate a character sheet for the character. if the character name is given, search the character and provide a character sheet based on the movie or show or game the character is mentioned in, or else use the given description. Ignore HP descriptions if mentioned.
+Respond in the following format, Nothing Extra, just the character sheet:
+
+       -->
+----------CHARACTER SHEET----------
+
+Name:
+Class:
+Race:
+
+Strength: 
+Agility: 
+Weakness: 
+Intelligence: 
+Charisma:
+
+[{A paragraph about the character's backstory}]
+
+Keep the details funny, but accurate to the character.
+
+<EXAMPLE>
+Spiderman
+
+
+----CHARACTER SHEET----
+Name: Spiderman
+Class: Agile Arachnid
+Race: Human (with a little bit of spider)
+
+Strength: 16 (+3)
+[Can lift twice his own weight, and has a knack for handy web-slinging.]
+
+Agility: 20 (+5)
+[Seriously, have you seen him parkour through New York City?]
+
+Weakness: 8 (-1)
+[Susceptible to emotional manipulation, and a well-known fear of spiders.]
+
+Intelligence: 14 (+2)
+[Above average, but he's no Tony Stark. He's got pretty great science skills, though.]
+
+Charisma: 12 (+1)
+[Quippy, charming, and a bit of a dork. But hey, that's why we love him!]
+
+
+Bitten by a radioactive spider at a science exhibit, young Peter Parker gained extraordinary powers and the great responsibility of fighting crime. Juggling school, a social life, and his superhero duties, Spiderman is always on the move, whether it's chasing down bad guys or taking cheeky selfies. Navigating the complexities of love and friendship while maintaining his secret identity, Spidey has a heart of gold and a deep-seated desire for justice. Just don't forget to remind him that "with great power comes great responsibility!"
+</EXAMPLE>
+
+`;
+
+export let loadingText = `
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            loading... 
+               
+               
+               
+               
+               
+               
+               
+               
+               `
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,24 @@
 <script>
 	import { onMount } from 'svelte';
-	import { ChatGroq } from "@langchain/groq";
-    let prompt = "";
-	let api_key = "";
-	let llm;
-    let messages = [];
+	import { initialText, gameLogo, characterSheetPrompt, loadingText } from '$lib';
 
-	let text1 = 'Press any key to continue... ';
-	let text2 = `
-██████╗░░█████╗░██████╗░
-██╔══██╗██╔══██╗██╔══██╗
-██║░░██║███████║██║░░██║
-██║░░██║██╔══██║██║░░██║
-██████╔╝██║░░██║██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░
-
-Dungeons, AI & Dragons.`;
+	let prompt = '';
+	let generatedImage = null;
+	let messages = [];
 	let displayText = '';
-	let characterContent = "";
-	let currentText = text1;
+	let characterContent = loadingText;
+	let currentText = initialText;
 	let index = 0;
-	let typingSpeed = 30; // Adjust speed (ms)
+	let typingSpeed = 30;
 	let showTextarea = false;
 	let showCharacterSheet = false;
+	let base64Image = '';
+	let isImageReady = false;
 
 	onMount(() => {
 		typeText();
-		load_api_key();
-		window.addEventListener('keydown', startText2); // Listen for any key press
+		window.addEventListener('keydown', startText2);
 	});
-
-	async function load_api_key() {
-		const response = await fetch('src/lib/api_key.txt');
-		api_key = await response.text();
-		initializeLLM();
-	}
-
-	function initializeLLM() {
-		llm = new ChatGroq({
-			apiKey: api_key,
-			model: "mixtral-8x7b-32768",
-			temperature: 1,
-			maxTokens: undefined,
-			maxRetries: 2,
-		});
-	}
 
 	function typeText(callback) {
 		if (index < currentText.length) {
@@ -59,41 +33,97 @@ Dungeons, AI & Dragons.`;
 	}
 
 	function startText2() {
-		window.removeEventListener('keydown', startText2); // Remove the key press listener
-		currentText = text2;
+		window.removeEventListener('keydown', startText2);
+		currentText = gameLogo;
 		index = 0;
-		displayText = ''; // Clear the previous text
-		//wait 1 second
-		typeText(() => (
+		displayText = '';
+		typeText(() =>
 			setTimeout(() => {
-				showTextarea = true
-			}, 1000))); // Show textarea after typing is complete
+				showTextarea = true;
+			}, 1000)
+		);
 	}
 
 	function getBoundValue() {
 		return showCharacterSheet ? characterContent : prompt;
 	}
 
-	async function sendMessage() {
+	async function sendMessage(systemPrompt) {
 		if (prompt.trim()) {
 			showCharacterSheet = true;
-			messages = [...messages, { role: "user", content: prompt }];
-			prompt = "";
 
-			const aiResponse = await llm.invoke([
-				{ role: "system", content: "" },
-				{ role: "user", content: messages[messages.length - 1].content },
-			]);
+			messages = [...messages, { role: 'user', content: prompt }];
 
-			messages = [...messages, { role: "AI", content: aiResponse.content }];
-			characterContent = aiResponse.content; // Set the AI response as the character sheet content
+			try {
+				const response = await fetch('/api/generate-text', {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ systemPrompt, messages })
+				});
+
+				if (response.ok) {
+					const data = await response.json();
+
+					messages = [...messages, { role: 'assistant', content: data.content }];
+					characterContent = data.content;
+
+					await handleGenerateImage('a pixel art potrait of ' + prompt);
+				} else {
+					console.error('Error in response:', response.statusText);
+				}
+			} catch (error) {
+				console.error('Error:', error);
+			}
 		}
 	}
-	
+
+	async function handleGenerateImage(imagePrompt) {
+		if (imagePrompt.trim()) {
+			const image = await generateImage(imagePrompt);
+			if (image) {
+				base64Image = image;
+			}
+		}
+	}
+
+	async function generateImage(imagePrompt) {
+		if (imagePrompt.trim() === '') return null;
+		try {
+			const response = await fetch('/api/generate-image', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ prompt: imagePrompt })
+			});
+
+			if (response.ok) {
+				setTimeout(() => {
+					isImageReady = true;
+				}, 1000);
+
+				const data = await response.json();
+				return data.image;
+			} else {
+				console.error('Error generating image');
+				return null;
+			}
+		} catch (error) {
+			console.error('Error:', error);
+			return null;
+		}
+	}
 </script>
 
 <div class="typing-container">
 	{displayText}<span class="cursor"></span>
+</div>
+
+<div class="image-container">
+	{#if base64Image}
+		<!-- svelte-ignore a11y_img_redundant_alt -->
+		<img src="data:image/png;base64,{base64Image}" alt="Generated Image" />
+	{:else if showCharacterSheet}
+		<img src="/src/lib/loading.gif" alt="Loading..." class="placeholder-image  {isImageReady ? 'pop-up' : ''}" />
+	{/if}
 </div>
 
 {#if showTextarea}
@@ -101,7 +131,8 @@ Dungeons, AI & Dragons.`;
 		<textarea
 			class="character-input {showCharacterSheet ? 'character-sheet' : ''}"
 			type="text"
-			on:keydown={(e) => e.key === "Enter" && sendMessage()}
+			on:keydown={(e) => e.key === 'Enter' && sendMessage(characterSheetPrompt)}
+			placeholder={showCharacterSheet ? loadingText : 'Name or Describe your character...'}
 			bind:value={prompt}
 			readonly={showCharacterSheet}
 		>
@@ -134,22 +165,51 @@ Dungeons, AI & Dragons.`;
 		cursor: default;
 	}
 
+	.image-container {
+		position: fixed;
+		top: 25px;
+		left: 50px;
+		width: 256px;
+		height: auto;
+		z-index: 1000;
+	}
+
+	.placeholder-image{
+		opacity: 0; /* Start hidden */
+		transform: scale(0.8); /* Start smaller */
+		transition: all 0.5s ease; /* Smooth transition for properties */
+		animation: fadeInPop 0.5s ease forwards;
+	}
+
+	.placeholder-image.pop-up{
+		opacity: 1;
+		transform: scale(1);
+	}
+
+	.image-container img {
+		width: 100%;
+		height: auto;
+		border: 2px solid white;
+		border-radius: 8px;
+		box-shadow: 0 0 10px rgba(255, 255, 255, 0.5);
+	}
 
 	.textarea-wrapper {
 		margin-top: 50px;
 		opacity: 0;
 		transform: scale(0.8);
 		animation: fadeInPop 0.5s ease forwards;
-		transition: transform 0.5s ease, width 0.5s ease, left 0.5s ease;
-		position: relative;
+		transition: all 0.5s ease; /* Smooth transition for repositioning */
 	}
 
 	.textarea-wrapper.character-sheet {
-		position: absolute;
-		top: 50%;
-		left: 10%;
-		transform: translateY(-50%);
-		width: 80%;
+		position: fixed;
+		bottom: 20px; /* Distance from the bottom of the screen */
+		left: 20px; /* Distance from the left of the screen */
+		margin-top: 0; /* Remove default margin */
+		width: 300px; /* Adjust width if needed */
+		transform: scale(1); /* Reset scaling */
+		opacity: 1; /* Ensure it's visible */
 	}
 
 	.character-input {
@@ -164,18 +224,27 @@ Dungeons, AI & Dragons.`;
 		font-size: 12px;
 		outline: none;
 		box-shadow: 0 0 10px white;
-		transition: width 0.5s ease, height 0.5s ease;
+		transition:
+			width 0.5s ease,
+			height 0.5s ease;
 	}
 
 	.character-input.character-sheet {
-		width: 100%;
-		height: 300px;
+		width: 100%; /* Adjust width to match wrapper */
+		height: 550px; /* Make it taller as a character sheet */
+		resize: none; /* Prevent resizing */
 		background-color: black;
 		color: white;
-		border: none;
-		box-shadow: none;
-		resize: none;
-		pointer-events: none; /* Make it non-editable */
+		border: 2px solid white;
+		border-radius: 8px;
+		padding: 10px;
+		font-family: monospace;
+		font-size: 14px;
+		box-shadow: 0 0 10px white;
+	}
+
+	.character-input.character-sheet:focus {
+		box-shadow: none; /* Prevent focus effects for the sheet */
 	}
 
 	.character-input::placeholder {
@@ -196,7 +265,8 @@ Dungeons, AI & Dragons.`;
 	}
 
 	@keyframes blink {
-		0%, 100% {
+		0%,
+		100% {
 			opacity: 1;
 		}
 		50% {
